22c82dddaff0ec67b82a9cfc89df5538f2670e05
GeunYeong <jgy091411@gmail.com>
2025-11-11 12:24:05 +0900

    [chore] : 신고 등록 API @Valid 적용

--- src/main/java/com/back/domain/report/report/controller/ReportController.java
+++ src/main/java/com/back/domain/report/report/controller/ReportController.java
@@ -5,6 +5,7 @@ import com.back.domain.report.report.dto.ReportResBody;
 import com.back.domain.report.report.service.ReportService;
 import com.back.global.rsData.RsData;
 import com.back.global.security.SecurityUser;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,7 +21,7 @@ public class ReportController {
     private final ReportService reportService;
 
     @PostMapping
-    public RsData<ReportResBody> postReport(@RequestBody ReportReqBody reportReqBody,
+    public RsData<ReportResBody> postReport(@Valid @RequestBody ReportReqBody reportReqBody,
                                             @AuthenticationPrincipal SecurityUser securityUser) {
         ReportResBody reportRes = reportService.postReport(reportReqBody, securityUser.getId());
         return RsData.success("신고가 등록되었습니다.", reportRes);
